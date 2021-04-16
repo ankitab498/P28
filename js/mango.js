d235e8e6351123122fca869a2d8c94ff732c2b18
ankitab498 <75402577+ankitab498@users.noreply.github.com>
2021-04-16 20:10:41 +0530

    Add files via upload

---
+++ js/mango.js
@@ -0,0 +1,29 @@
+class mango{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		//load the image
+
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var mangoPos=this.body.position;	
+		push()
+		translate(mangoPos.x, mangoPos.y);
+		rotate(this.body.angle)
+		fill(255,0,255)
+		//image representation
+
+		pop()
+ }
+}
